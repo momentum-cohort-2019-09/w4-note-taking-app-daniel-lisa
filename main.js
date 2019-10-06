5ed6055a28b7bc659b75a6b5c81a6495bf95cb8c
danielwalker <dawalker2017@outlook.com>
2019-10-06 16:32:27 -0400

    10.06.19 04:31PM Checkpoint; Added majority of Javascript, just working out the kinks.

--- main.js
+++ main.js
@@ -1,65 +1,129 @@
 /* globals fetch */
 
-function query(selector) { //assigned the selector of 'query' to a function.//
-    return document.querySelector(selector)
-}
+const app = {
 
-function queryAll(selector) { //assigned the selector of 'queryAll' to a function.//
-    return document.querySelectorAll(selector)
-}
+    data: {
+        credentials: {
+            username: sessionStorage.getItem('username'),
+            password: sessionStorage.getItem('password')
+        },
+        notes: []
+    },
 
-function addAuthHeader(headers) {
-    if (!headers) {
-        headers = {};
-    }
-
-    return Object.assign({}, headers, {
-        Authorization: "Basic Team410:password"
-    });
-}
-
-function displayAllNotes() {
-    fetch("https://notes-api.glitch.me/api/notes", {
-            headers: {
-                Authorization: "Basic " + btoa("Team410:password")
-            }
-        })
-        .then(response => response.json())
-        .then(json => {
-            for (let note of json.notes) {
-                let noteDiv = document.querySelector("#notes-container");
-
-                noteDiv.innerHTML +=
-                    `<div class="box1"></div>
-                        <div id="actualNote">
-                            <div class="noteTitle">
-                                <h3>${note.title}</h3>
-                            </div>
-                            <div class="noteContent">
-                                <p>${note.text}</p>
-                            </div>
-                            <form id="deleteButtonForm">
-                                <button class="deleteNote">Delete</button>
-                            <form>
-                        </div>`
-            }
-        });
-}
-
-query("#addNewNoteForm").addEventListener('submit', function(addNewNote) {
-    event.preventDefault()
-    fetch("https://notes-api.glitch.me/api/notes", {
-        method: 'POST',
-        body: JSON.stringify({
-            title: title,
-            text: text
-        }),
-        headers: {
-            'Authorization': "Basic " + btoa("Team410:password"),
-            'Content-Type': 'application/json'
-
+    setCredentials: function(username, password) {
+        this.data.credentials = {
+            username: username,
+            password: password
         }
-    })
-})
+        sessionStorage.setItem('username', username)
+        sessionStorage.setItem('password', password)
+    },
 
-displayAllNotes();
+    addAuthorization: function(headers) {
+        if (!headers) { headers = {} }
+        return Object.assign({}, headers, {
+            'Authorization': 'Basic ' + btoa(`Team410:password`)
+        })
+    },
+
+    login: function(username, password) {
+        fetch('https://notes-api.glitch.me/api/notes', {
+                headers: {
+                    'Authorization': 'Basic ' + btoa(`Team410:password`)
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    this.setCredentials(username, password)
+                    this.render()
+                } else {
+                    document.getElementById('login-error').innerText = 'That is not a valid username and password.'
+                }
+            })
+    },
+
+    displayAllNotes: function() {
+        return fetch("https://notes-api.glitch.me/api/notes", {
+                headers: app.addAuthorization()
+            })
+            .then(response => response.json())
+            .then(notesData => {
+                this.data.notes = notesData.notes
+            })
+    },
+
+    addNewNote: function(title, text) {
+        return fetch("https://notes-api.glitch.me/api/notes", {
+                method: 'POST',
+                body: JSON.stringify({
+                    "title": title,
+                    "text": text
+                }),
+                headers: this.addAuthorization({
+                    'Content-Type': 'application/json'
+                })
+            })
+            .then(response => {})
+    },
+
+    deleteNote: function(noteId) {
+        return fetch("https://notes-api.glitch.me/api/notes" + noteId, {
+                method: 'DELETE',
+                headers: this.addAuthorization({
+                    'Content-Type': 'application/json'
+                })
+            })
+            .then(response => {})
+    },
+
+    writeNoteToHTML: function(note) {
+        return `
+        <div id="actualNote">
+
+            <div class="noteTitle">
+                <h3>${note.title}</h3>
+            </div>
+
+            <div class="noteContent">
+                <p>${note.text}</p>
+            </div>
+
+            <div class="timeOfNewNote">
+                Timestamp: ${note.updated}
+            </div>
+                <button class="deleteNoteButton id="${note.id}">Delete Note</button>
+        </div>`
+    },
+
+    renderAllNotes: function() {
+        document.getElementById('notes-container').innerHTML = this.data.notes.map(this.writeNoteToHTML).join('\n')
+    }
+}
+
+function main() {
+    app.renderAllNotes()
+
+    document.querySelector('#addNewNoteForm').addEventListener('submit', function(event) {
+        event.preventDefault()
+
+        const noteFormData = new FormData(noteForm)
+        const title = noteFormData.get('addNewNoteTitle')
+        const text = noteFormData.get('addNewNoteText')
+
+        app.addNewNote(title, text)
+        app.displayAllNotes()
+        app.renderAllNotes()
+    })
+
+    document.getElementById('notes-container').addEventListener('click', function(event) {
+        event.preventDefault()
+
+        const noteId = event.target.id
+
+        app.deleteNote(noteId)
+        app.displayAllNotes()
+        app.renderAllNotes()
+    })
+}
+
+main()
